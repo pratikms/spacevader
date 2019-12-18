2cb638548da955af49e46bdd9602bb6814c983a0
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2019-12-19 00:42:38 +0530

    Add player to screen

--- src/spacevaders/main.py
+++ src/spacevaders/main.py
@@ -6,8 +6,28 @@ pygame.init()
 # Create a screen
 screen = pygame.display.set_mode((800, 600))
 
+# Set title and icon
+pygame.display.set_caption('Spacevaders')
+# TODO: Check icon visibility in Windows
+icon = pygame.image.load('icons/ufo.png')
+pygame.display.set_icon(icon)
+
+# Player
+player_icon = pygame.image.load('icons/player.png')
+player_x = 370
+player_y = 480
+
+def player():
+    screen.blit(player_icon, (player_x, player_y))
+
 running = True
 while running:
+    
+    screen.fill((0, 0, 0))
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    player()
+    pygame.display.update()
