a3fc29c1bf803a21b5936f5d6b6d66fe7bd5d53f
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2019-12-19 00:15:13 +0530

    Create game window

---
+++ src/spacevaders/main.py
@@ -0,0 +1,13 @@
+import pygame
+
+# Initialize pygame
+pygame.init()
+
+# Create a screen
+screen = pygame.display.set_mode((800, 600))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
